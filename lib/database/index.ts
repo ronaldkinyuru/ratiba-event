191f874f3a9a1fb15041e26703d8dad7f4bd3ff9
ronaldkinyuru <ronaldkinyuru@gmail.com>
2024-10-17 10:48:28 +0300

    moongoose

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,19 +2,32 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// Extending the NodeJS global type
+declare global {
+  var mongoose: MongooseCache; // This is necessary to avoid "global" typing issues
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: 'ratiba',
     bufferCommands: false,
-  })
+  });
 
   cached.conn = await cached.promise;
 
+  // Cache the mongoose connection in global to reuse it in the future
+  global.mongoose = cached;
+
   return cached.conn;
-}
+};
